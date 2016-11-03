e1fa5bf16e12d22a55ad588e3e09d9b4029b09ff
Beau Harper <clioharp@usc.edu>
2016-11-03 15:44:03 +0200

    Add permissions and subscriptions lists

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -1,50 +1,54 @@
-import React, { Component } from 'react'
-import { Scene, Router } from 'react-native-router-flux'
-import Styles from './Styles/NavigationContainerStyle'
-import NavigationDrawer from './NavigationDrawer'
-import NavItems from './NavItems'
+import React, { Component } from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import Styles from './Styles/NavigationContainerStyle';
+import NavigationDrawer from './NavigationDrawer';
+import NavItems from './NavItems';
 
 // screens identified by the router
-import PresentationScreen from '../Containers/PresentationScreen'
-import AllComponentsScreen from '../Containers/AllComponentsScreen'
-import UsageExamplesScreen from '../Containers/UsageExamplesScreen'
-import LoginScreen from '../Containers/LoginScreen'
-import ListviewExample from '../Containers/ListviewExample'
-import ListviewGridExample from '../Containers/ListviewGridExample'
-import ListviewSectionsExample from '../Containers/ListviewSectionsExample'
+import PresentationScreen from '../Containers/PresentationScreen';
+import AllComponentsScreen from '../Containers/AllComponentsScreen';
+import UsageExamplesScreen from '../Containers/UsageExamplesScreen';
+import LoginScreen from '../Containers/LoginScreen';
+import ListviewExample from '../Containers/ListviewExample';
+import ListviewGridExample from '../Containers/ListviewGridExample';
+import ListviewSectionsExample from '../Containers/ListviewSectionsExample';
 // No maps by default for now
 // import MapviewExample from '../Containers/MapviewExample'
-import APITestingScreen from '../Containers/APITestingScreen'
-import ThemeScreen from '../Containers/ThemeScreen'
-import DeviceInfoScreen from '../Containers/DeviceInfoScreen'
-import HomeScreen from '../Containers/HomeScreen'
+import APITestingScreen from '../Containers/APITestingScreen';
+import ThemeScreen from '../Containers/ThemeScreen';
+import DeviceInfoScreen from '../Containers/DeviceInfoScreen';
+import HomeScreen from '../Containers/HomeScreen';
+import PermissionsList from '../Containers/PermissionsList';
+import SubscriptionsList from '../Containers/SubscriptionsListScreen';
 
 /* **************************
 * Documentation: https://github.com/aksonov/react-native-router-flux
 ***************************/
 
 class NavigationRouter extends Component {
-  render () {
+  render() {
     return (
       <Router>
-        <Scene key='drawer' component={NavigationDrawer} open={false}>
-          <Scene key='drawerChildrenWrapper' navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
-            <Scene initial key='presentationScreen' component={PresentationScreen} title='Ignite' renderLeftButton={NavItems.hamburgerButton} />
-            <Scene key='componentExamples' component={AllComponentsScreen} title='Components' />
-            <Scene key='usageExamples' component={UsageExamplesScreen} title='Usage' rightTitle='Example' onRight={() => window.alert('Example Pressed')} />
-            <Scene key='login' component={LoginScreen} title='Login' hideNavBar />
-            <Scene key='listviewExample' component={ListviewExample} title='Listview Example' />
-            <Scene key='listviewGridExample' component={ListviewGridExample} title='Listview Grid' />
-            <Scene key='listviewSectionsExample' component={ListviewSectionsExample} title='Listview Sections' />
-            <Scene key='apiTesting' component={APITestingScreen} title='API Testing' />
-            <Scene key='theme' component={ThemeScreen} title='Theme' />
-            <Scene key='deviceInfo' component={DeviceInfoScreen} title='Device Info' />
-            <Scene key='homeScreen' component={HomeScreen} title='Home Screen' rightTitle='Test' onRight={() => window.alert('Example Pressed')} />
+        <Scene key="drawer" component={NavigationDrawer} open={false}>
+          <Scene key="drawerChildrenWrapper" navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
+            <Scene initial key="presentationScreen" component={PresentationScreen} title="Ignite" renderLeftButton={NavItems.hamburgerButton} />
+            <Scene key="componentExamples" component={AllComponentsScreen} title="Components" />
+            <Scene key="usageExamples" component={UsageExamplesScreen} title="Usage" rightTitle="Example" onRight={() => window.alert('Example Pressed')} />
+            <Scene key="login" component={LoginScreen} title="Login" hideNavBar />
+            <Scene key="listviewExample" component={ListviewExample} title="Listview Example" />
+            <Scene key="listviewGridExample" component={ListviewGridExample} title="Listview Grid" />
+            <Scene key="listviewSectionsExample" component={ListviewSectionsExample} title="Listview Sections" />
+            <Scene key="apiTesting" component={APITestingScreen} title="API Testing" />
+            <Scene key="theme" component={ThemeScreen} title="Theme" />
+            <Scene key="deviceInfo" component={DeviceInfoScreen} title="Device Info" />
+            <Scene key="homeScreen" component={HomeScreen} title="Home Screen" />
+            <Scene key="permissionsList" component={PermissionsList} title="Permissions List" />
+            <Scene key="subscriptionsList" component={SubscriptionsList} title="Subscriptions List" />
           </Scene>
         </Scene>
       </Router>
-    )
+    );
   }
 }
 
-export default NavigationRouter
+export default NavigationRouter;
