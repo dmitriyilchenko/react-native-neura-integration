5e8a6cbfe65fd29af94a93a2aee93a79230c889d
Beau Harper <clioharp@usc.edu>
2016-11-03 15:44:41 +0200

    Add neura app uuid and secrets and reference to root view controller

--- ios/neuraReactNativeiOS/AppDelegate.m
+++ ios/neuraReactNativeiOS/AppDelegate.m
@@ -11,13 +11,20 @@
 
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
+#import <NeuraSDK/NeuraSDK.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  //MARK: Neura app key and secret
+  [[NeuraSDK sharedInstance] setAppUID:@"d272cd697687a86cd49330ac3c1a6479942383eee1a5dee56bb2c0f42a74b6a7"];
+  [[NeuraSDK sharedInstance] setAppSecret:@"aa7d305c23f446df4c6118302a8619b555b1d19090853a3e808192ff7b98354c"];
+  
+  
   NSURL *jsCodeLocation;
-
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
@@ -30,6 +37,11 @@
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
+  
+  // Set reference to class property
+  self.rootViewController = rootViewController;
+  
+  
   [self.window makeKeyAndVisible];
   return YES;
 }
