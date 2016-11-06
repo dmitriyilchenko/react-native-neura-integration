de7209030de14394aed454d7ed805686b6c2dfcb
Beau Harper <clioharp@usc.edu>
2016-11-06 20:01:19 +0200

    Add all SDK functions to bridge (untested)

--- ios/neuraReactNativeiOS/NeuraSDKREACT.m
+++ ios/neuraReactNativeiOS/NeuraSDKREACT.m
@@ -30,17 +30,152 @@ RCT_EXPORT_METHOD(authenticateWithPermissions:(RCTResponseSenderBlock)callback)
                                                withHandler:^(NSString *token, NSString *error) {
                                                  if (token) {
                                                    RCTLogInfo(@"%@", token)
-//                                                   [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kIsUserLogin];
+                                                   //                                                   [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kIsUserLogin];
                                                    callback(@[token, [NSNull null]]);
                                                  }  else {
                                                    RCTLogInfo(@"login error = %@", error);
-//                                                   [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kIsUserLogin];
+                                                   //                                                   [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kIsUserLogin];
                                                    callback(@[[NSNull null], error]);
                                                  }
                                                }];
   });
 }
 
+RCT_EXPORT_METHOD(logout)
+{
+  [[NeuraSDK sharedInstance] logout];
+}
+
+RCT_EXPORT_METHOD(getVersion: (RCTResponseSenderBlock)callback)
+{
+  callback(@[[[NeuraSDK sharedInstance] getVersion]]);
+}
+
+RCT_EXPORT_METHOD(openNeuraSettingsPanel)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [[NeuraSDK sharedInstance] openNeuraSettingsPanel];
+  });
+}
+
+RCT_EXPORT_METHOD(getSubscriptions: (RCTResponseSenderBlock)callback)
+{
+  //See what user is subscribed to now
+  [[NeuraSDK sharedInstance] getSubscriptions:^(NSDictionary *responseData, NSString *error) {
+    if (error) {
+      callback(@[[NSNull null], error]);
+    }
+    NSArray *subscriptionsArray = responseData[@"items"];
+    callback(@[subscriptionsArray, [NSNull null]]);
+  }];
+}
+
+RCT_EXPORT_METHOD(getPermissions: (RCTResponseSenderBlock)callback)
+{
+  [[NeuraSDK sharedInstance]getAppPermissionsWithHandler:^(NSArray *permissionsArray, NSString *error) {
+    if (error) {
+      callback(@[[NSNull null], error]);
+    }
+    callback(@[permissionsArray, [NSNull null]]);
+  }];
+}
+
+RCT_EXPORT_METHOD(isMissingDataForEvent:(NSString *)eventName callback:(RCTResponseSenderBlock)callback)
+{
+  callback(@[@([[NeuraSDK sharedInstance] isMissingDataForEvent: eventName])]);
+}
+
+RCT_EXPORT_METHOD(getMissingDataForEvent:(NSString *)eventName callback:(RCTResponseSenderBlock)callback)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [[NeuraSDK sharedInstance]getMissingDataForEvent:eventName withHandler:^(NSDictionary *responseData, NSString *error) {
+      if (error) {
+        callback(@[[NSNull null], error]);
+        return;
+      }
+      callback(@[responseData, [NSNull null]]);
+    }];
+  });
+}
+
+RCT_EXPORT_METHOD(subscribeToEvent:(NSString *)eventName callback:(RCTResponseSenderBlock)callback)
+{
+  [[NeuraSDK sharedInstance]subscribeToEvent:eventName identifier:[NSString stringWithFormat:@"_%@",eventName] webHookID:nil state:[NSString stringWithFormat:@"state_%@",eventName] completion:^(NSDictionary *responseData, NSString *error) {
+    if (error) {
+      callback(@[[NSNull null], error]);
+      return;
+    }
+    callback(@[responseData, [NSNull null]]);
+  }];
+}
+
+RCT_EXPORT_METHOD(removeSubscriptionWithIdentifier:(NSString *)eventName callback:(RCTResponseSenderBlock)callback)
+{
+  [[NeuraSDK sharedInstance]removeSubscriptionWithIdentifier:[NSString stringWithFormat:@"_%@",eventName] complete:^(NSDictionary *responseData, NSString *error) {
+    if (error) {
+      callback(@[[NSNull null], error]);
+      return;
+    }
+    callback(@[responseData, [NSNull null]]);
+  }];
+}
+
+RCT_EXPORT_METHOD(getSupportedCapabilitiesListWithHandler:(RCTResponseSenderBlock)callback)
+{
+  [[NeuraSDK sharedInstance] getSupportedCapabilitiesListWithHandler:^(NSDictionary *responseData, NSString *error) {
+    if (error){
+      callback(@[[NSNull null], error]);
+      return;
+    }
+    if (responseData) {
+      NSArray *list = [responseData[@"items"] valueForKey:@"name"];
+      callback(@[list, [NSNull null]]);
+    }}];
+}
+
+RCT_EXPORT_METHOD(hasDeviceWithCapability:(NSString *)capability callback:(RCTResponseSenderBlock)callback)
+{
+  [[NeuraSDK sharedInstance]
+   hasDeviceWithCapability:capability
+   withHandler:^(NSDictionary *responseData, NSString *error) {
+     if (error) {
+       callback(@[[NSNull null], error]);
+       return;
+     }
+     callback(@[responseData, [NSNull null]]);
+   }];
+}
+
+RCT_EXPORT_METHOD(addDeviceWithCapability:(NSString *)capability deviceName:(NSString *)deviceName callback:(RCTResponseSenderBlock)callback)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [[NeuraSDK sharedInstance]
+     addDeviceWithCapability:capability
+     deviceName:deviceName
+     withHandler:^(NSDictionary *responseData, NSString *error) {
+       if (error) {
+         callback(@[[NSNull null], error]);
+         return;
+       }
+       callback(@[responseData, [NSNull null]]);
+     }];
+  });
+}
+
+RCT_EXPORT_METHOD(getUserSituationForTimeStamp:(NSDate *)timestamp contextual:(BOOL *)contextual callback:(RCTResponseSenderBlock)callback)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [[NeuraSDK sharedInstance]
+     getUserSituationForTimeStamp:timestamp
+     contextual: contextual
+     withHandler:^(NSDictionary *responseData, NSString *error) {
+       if (error) {
+         callback(@[[NSNull null], error]);
+         return;
+       }
+       callback(@[responseData, [NSNull null]]);
+     }];
+  });
+}
 
 @end
-
