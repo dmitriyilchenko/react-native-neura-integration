4df2f42829056116e6b9474f57cfe5fc3be8e0da
Beau Harper <clioharp@usc.edu>
2016-11-10 12:25:46 +0200

    Return appropriate SDK manager for platform

--- App/Lib/NeuraSDKManager.js
+++ App/Lib/NeuraSDKManager.js
@@ -1,11 +1,9 @@
 import { NativeModules, Platform } from 'react-native';
 
-// const neuraSDKBridge = Platform.os === 'ios' ? NativeModules.NeuraSDKManager : NativeModules.NeuraSDKManagerAndroid;
+const neuraSDKBridge = Platform.os === 'ios' ? NativeModules.NeuraSDKManager : NativeModules.NeuraSDKManagerAndroid;
 
-const neuraSDKBridge = NativeModules.NeuraSDKManagerAndroid;
-neuraSDKBridge.initConnection();
-// if (Platform.os === 'android') {
-//   neuraSDKBridge.initConnection();
-// }
+if (Platform.os !== 'ios') {
+  neuraSDKBridge.initConnection();
+}
 
 export default neuraSDKBridge;
