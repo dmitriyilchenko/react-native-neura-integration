a3b8005a5d4c2d9469bc6b144918755b3f87f677
Beau Harper <clioharp@usc.edu>
2016-11-16 13:48:04 +0200

    Delete all unused views

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Styles from './Styles/NavigationContainerStyle';
-import NavigationDrawer from './NavigationDrawer';
-import NavItems from './NavItems';
+
 
 // screens identified by the router
-import PresentationScreen from '../Containers/PresentationScreen';
-import AllComponentsScreen from '../Containers/AllComponentsScreen';
-import UsageExamplesScreen from '../Containers/UsageExamplesScreen';
-import LoginScreen from '../Containers/LoginScreen';
-import ListviewExample from '../Containers/ListviewExample';
-import ListviewGridExample from '../Containers/ListviewGridExample';
-import ListviewSectionsExample from '../Containers/ListviewSectionsExample';
+
 // No maps by default for now
 // import MapviewExample from '../Containers/MapviewExample'
-import APITestingScreen from '../Containers/APITestingScreen';
-import ThemeScreen from '../Containers/ThemeScreen';
-import DeviceInfoScreen from '../Containers/DeviceInfoScreen';
+
 import HomeScreen from '../Containers/HomeScreen';
 import PermissionsList from '../Containers/PermissionsList';
 import SubscriptionsList from '../Containers/SubscriptionsListScreen';
@@ -32,25 +23,13 @@ class NavigationRouter extends Component {
   render() {
     return (
       <Router>
-        <Scene key="drawer" component={NavigationDrawer} open={false}>
-          <Scene key="drawerChildrenWrapper" navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
-            <Scene initial key="presentationScreen" component={PresentationScreen} title="Ignite" renderLeftButton={NavItems.hamburgerButton} />
-            <Scene key="componentExamples" component={AllComponentsScreen} title="Components" />
-            <Scene key="usageExamples" component={UsageExamplesScreen} title="Usage" rightTitle="Example" onRight={() => window.alert('Example Pressed')} />
-            <Scene key="login" component={LoginScreen} title="Login" hideNavBar />
-            <Scene key="listviewExample" component={ListviewExample} title="Listview Example" />
-            <Scene key="listviewGridExample" component={ListviewGridExample} title="Listview Grid" />
-            <Scene key="listviewSectionsExample" component={ListviewSectionsExample} title="Listview Sections" />
-            <Scene key="apiTesting" component={APITestingScreen} title="API Testing" />
-            <Scene key="theme" component={ThemeScreen} title="Theme" />
-            <Scene key="deviceInfo" component={DeviceInfoScreen} title="Device Info" />
-            <Scene key="homeScreen" component={HomeScreen} title="Home Screen" />
-            <Scene key="permissionsList" component={PermissionsList} title="Permissions List" />
-            <Scene key="subscriptionsList" component={SubscriptionsList} title="Subscriptions List" />
-            <Scene key="devicesScreen" component={DevicesScreen} title="Devices" />
-            <Scene key="capabilitiesList" component={CapabilitiesList} title="Capabilities List" />
-            <Scene key="devicesList" component={DevicesList} title="Devices List" />
-          </Scene>
+        <Scene key="drawerChildrenWrapper" navigationBarStyle={Styles.navBar} titleStyle={Styles.title}>
+          <Scene key="homeScreen" component={HomeScreen} title="Home Screen" />
+          <Scene key="permissionsList" component={PermissionsList} title="Permissions List" />
+          <Scene key="subscriptionsList" component={SubscriptionsList} title="Subscriptions List" />
+          <Scene key="devicesScreen" component={DevicesScreen} title="Devices" />
+          <Scene key="capabilitiesList" component={CapabilitiesList} title="Capabilities List" />
+          <Scene key="devicesList" component={DevicesList} title="Devices List" />
         </Scene>
       </Router>
     );
