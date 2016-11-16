02e9482ae210e1123b9a2d98ce415a2737cd9749
Beau Harper <clioharp@usc.edu>
2016-11-16 13:45:34 +0200

    Permissions list styling

--- App/Containers/Styles/PermissionsListStyle.js
+++ App/Containers/Styles/PermissionsListStyle.js
@@ -1,6 +1,31 @@
-import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { StyleSheet } from 'react-native';
+import { ApplicationStyles, Metrics, Colors } from '../../Themes/';
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  ...ApplicationStyles.screen,
+  container: {
+    flex: 1,
+    marginTop: Metrics.navBarHeight,
+    backgroundColor: Colors.background,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#F6F6F6',
+  },
+  boldLabel: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    color: Colors.snow,
+    textAlign: 'center',
+    marginBottom: Metrics.smallMargin,
+  },
+  label: {
+    textAlign: 'center',
+    color: Colors.black,
+  },
+  listContent: {
+    marginTop: Metrics.baseMargin,
+  },
+});
