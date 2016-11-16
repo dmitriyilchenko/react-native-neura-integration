4f1788177b9a3c8ddbcc6f5f8bf0171deb7942ea
Beau Harper <clioharp@usc.edu>
2016-11-16 13:39:42 +0200

    Delete extraneous image references

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -1,32 +1,9 @@
 // leave off @2x/@3x
 const images = {
-  bg: require('../Images/BG.png'),
-  igniteLogo: require('../Images/ignite_logo.png'),
-  ir: require('../Images/ir.png'),
+
   neuraSdkDemoLogo: require('../Images/neura_sdk_demo_logo.png'),
   neuraSymbolBottomElement: require('../Images/neura_symbol_bottom_element.png'),
   neuraSymbolTopElement: require('../Images/neura_symbol_top_element.png'),
-  tileBg: require('../Images/tile_bg.png'),
-  topLogo: require('../Images/top_logo.png'),
-  bg: require('../Images/BG.png'),
-  igniteLogo: require('../Images/ignite_logo.png'),
-  ir: require('../Images/ir.png'),
-  neuraSymbolBottomElement: require('../Images/neura_symbol_bottom_element.png'),
-  neuraSymbolTopElement: require('../Images/neura_symbol_top_element.png'),
-  tileBg: require('../Images/tile_bg.png'),
-  topLogo: require('../Images/top_logo.png'),
-  bg: require('../Images/BG.png'),
-  igniteLogo: require('../Images/ignite_logo.png'),
-  ir: require('../Images/ir.png'),
-  neuraSymbolBottomElement: require('../Images/neura_symbol_bottom_element.png'),
-  neuraSymbolTopElement: require('../Images/neura_symbol_top_element.png'),
-  tileBg: require('../Images/tile_bg.png'),
-  topLogo: require('../Images/top_logo.png'),
-  logo: require('../Images/ir.png'),
-  clearLogo: require('../Images/top_logo.png'),
-  ignite: require('../Images/ignite_logo.png'),
-  tileBg: require('../Images/tile_bg.png'),
-  background: require('../Images/BG.png')
 }
 
 export default images
