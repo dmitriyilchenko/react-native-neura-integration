b3414ae26285b9bee330c1131c9a76e4b6233121
Adi Erel <adi.t.erel@gmail.com>
2018-07-17 07:06:36 -0400

    finalize iOS method adding

--- ios/NeuraSDKManager/NeuraSDKManager.m
+++ ios/NeuraSDKManager/NeuraSDKManager.m
@@ -60,7 +60,7 @@ RCT_REMAP_METHOD(logOut,
                  logOutResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-  if ([NeuraSDK.shared isAuthenticated]) {
+  if (![NeuraSDK.shared isAuthenticated]) {
     resolve(@(YES));
   }
   dispatch_async(dispatch_get_main_queue(), ^{
@@ -71,22 +71,61 @@ RCT_REMAP_METHOD(logOut,
         NeuraAPIError *err = result.error;
         reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);     
       }
-    }]
+    }];
+  });
+}
+
+RCT_REMAP_METHOD(addWebhookSubsciption,
+                 eventName:(NSString *)eventName
+                 identifier:(NSString *)identifier
+                 webhookId:(NSString *)WebhookId
+                 tagEngagementFeatureResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+      NSubscription *subscription = [[NSubscription alloc] initWithEventName:eventName identifier:identifier webhookId:WebhookId method:NSubscriptionMethodWebhook];
+        [NeuraSDK.shared addSubscription:(subscription) callback:^(NeuraAddSubscriptionResult * _Nonnull result){
+            if (result.success) {
+                resolve(@(YES));
+            } else {
+                NeuraAPIError *err = result.error;
+                reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);
+            }
+        }];
+    });
+}
+
+RCT_REMAP_METHOD(addPushSubscription,
+                 eventName:(NSString *)eventName
+                 pushIdentifier:(NSString *)pushIdentifier
+                 tagEngagementFeatureResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    NSubscription *subscription = [[NSubscription alloc] initWithEventName:eventName identifier:pushIdentifier webhookId:nil method:NSubscriptionMethodPush];
+    [NeuraSDK.shared addSubscription:(subscription) callback:^(NeuraAddSubscriptionResult * _Nonnull result){
+      if (result.success) {
+        resolve(@(YES));
+      } else {
+        NeuraAPIError *err = result.error;
+        reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);
+      }
+    }];
   });
 }
 
 RCT_REMAP_METHOD(simulateEvent,
+                 eventName:(NSString *)eventName
                  simulateEventResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
   dispatch_async(dispatch_get_main_queue(), ^{
-    NEventName enumEventName = [NEvent enumForEventName:"userStartedDriving"];
+    NEventName enumEventName = [NEvent enumForEventName:eventName];
     [NeuraSDK.shared simulateEvent:(enumEventName) callback:^(NeuraAPIResult * result){
       if (result.success) {
         resolve(@(YES));
       } else {
         NeuraAPIError *err = result.error;
-
         reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);
       }
     }];
@@ -96,32 +135,47 @@ RCT_REMAP_METHOD(simulateEvent,
 
 // follow https://dev.theneura.com/pages/how-to-use-engagement-api/ for details on how to utilize the Insights API
 RCT_REMAP_METHOD(tagEngagementAttempt,
+                 featureName:(NSString *)featureName
+                 value:(nullable NSString *)value
+                 instanceId:(nullable NSString *)instanceId
                  tagEngagementAttemptResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
   dispatch_async(dispatch_get_main_queue(), ^{
     NSError *error = nil;
-
-    BOOL success = [NeuraSDK.shared tagEngagementAttempt:"featureName" value:"ABTestId" instanceId:nil error:&error];
+    BOOL success = [NeuraSDK.shared tagEngagementAttempt:featureName value:nil instanceId:nil error:&error];
     if (success) {
       resolve(@(YES));
     } else {
-      reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);
+      reject([NSString stringWithFormat: @"%lu", (long)error.code], error.localizedDescription, error);
     }
   });
 }
 
 RCT_REMAP_METHOD(tagEngagementFeature,
+                 featureName:(NSString *)featureName
+                 action:(NSString *)action
+                 value:(nullable NSString *)value
+                 instanceId:(nullable NSString *)instanceId
                  tagEngagementFeatureResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
   dispatch_async(dispatch_get_main_queue(), ^{
+    NSDictionary *enumValueByName = @{
+      @"FeatureActionSnooze":   @(NEngagementFeatureActionSnooze),
+      @"FeatureActionSuccess":  @(NEngagementFeatureActionSuccess),
+      @"FeatureActionRejected": @(NEngagementFeatureActionRejected),
+      @"FeatureActionOpposite": @(NEngagementFeatureActionOpposite),
+      @"FeatureActionClose":    @(NEngagementFeatureActionClose)
+    };
+    NSNumber *enumValueNumber = enumValueByName[action];
+
     NSError *error = nil;
-    BOOL success = [NeuraSDK.shared tagEngagementFeature:"featureName" action: NEngagementFeatureActionSuccess value:valueField.text instanceId:instanceIdField.text error:&error];
+    BOOL success = [NeuraSDK.shared tagEngagementFeature:featureName action:(NEngagementFeatureAction)[enumValueNumber integerValue] value:value instanceId:instanceId error:&error];
     if (success) {
       resolve(@(YES));
     } else {
-      reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);
+      reject([NSString stringWithFormat: @"%lu", (long)error.code], error.localizedDescription, error);
     }
   });
 }
